40ab58a30856703b0c8ab9b50abd4d416377c888
JohannLiebert1130 <672016243@qq.com>
2018-05-14 22:00:38 +0800

    add CRUD methods

--- src/model/teachers/teacher.py
+++ src/model/teachers/teacher.py
@@ -3,14 +3,16 @@ from src.model.users.user import User
 
 
 class Teacher(User):
-    def __init__(self, user_id, name, p_id, folk=None, political_status=None,
-                 school=None, position=None, phone=None):
-        super().__init__(user_id, 1, name, p_id, folk, political_status, school, phone)
+    def __init__(self, user_id, name, p_id=None, gender=None, birthday=None, birth_place=None,
+                 folk=None, political_status=None, school=None, position=None, phone=None):
+        super().__init__(user_id, 1, name, p_id, gender, birthday, birth_place, folk,
+                         political_status, school, phone)
         self.position = position
 
     @staticmethod
-    def create_teacher(user_id, name, p_id, folk=None, political_status=None,
-                 school=None, position=None, phone=None):
+    def create_teacher(user_id, name, p_id=None, gender=None, birthday=None, birth_place=None,
+                       folk=None, political_status=None, school=None, position=None, phone=None):
+
         sql = """
               SELECT * FROM teachers
               WHERE user_id = %s OR p_id = %s
@@ -22,24 +24,81 @@ class Teacher(User):
             print("The user id you used to register already exists.")
             return False
 
-        Teacher(user_id, name, p_id, folk, political_status, school, position, phone).save_to_db()
+        Teacher(user_id, name, p_id, gender, birthday, birth_place, folk,
+                political_status, school, position, phone).save_to_db()
         return True
+
+    @staticmethod
+    def read_teacher(user_id):
+        sql = """
+              SELECT * FROM teachers
+              WHERE user_id = %s
+            """
+
+        user_data = Database.query(sql, user_id)
+
+        if user_data:
+            user_data = list(user_data[0])
+            user_data[6] = str(user_data[6])
+
+            data = [user_data[1]]
+            data.extend(user_data[3:])
+
+            return Teacher(*user_data)
+        else:
+            print("user do not exist!")
+            return None
+
+    @staticmethod
+    def modify_teacher(user_id, name, p_id=None, gender=None, birthday=None, birth_place=None,
+                       folk=None, political_status=None, school=None, position=None, phone=None):
+
+        sql = """
+              SELECT * FROM teachers
+              WHERE user_id = %s
+            """
+        user_data = Database.query(sql, user_id)
+
+        if user_data:
+            Teacher(user_id, name, p_id, gender, birthday, birth_place, folk,
+                    political_status, school, position, phone).save_to_db()
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def delete_account(user_id):
+        sql = """
+                  DELETE FROM teachers 
+                  WHERE user_id = (%s)
+                  """
+        try:
+            Database.data_handle(sql, user_id)
+        except:
+            print('Delete account failed!')
+            return False
+        else:
+            return True
 
     def save_to_db(self):
         sql = """
             INSERT INTO teachers(user_id, user_type, name, p_id, gender, birthday, birth_place, folk,
              political_status, school, position, phone)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            ON DUPLICATE KEY UPDATE folk = %s, political_status = %s, 
-            school = %s, position = %s, phone = %s
+            ON DUPLICATE KEY UPDATE name = %s, p_id = %s, gender = %s, birthday = %s,
+            birth_place = %s, folk = %s, political_status = %s, school = %s, position = %s, phone = %s
             """
 
-        Database.data_handle(sql, self.user_id, self.user_type, self.name, self.p_id, self.gender, self.birthday,
-                             self.birth_place, self.folk, self.political_status, self.school, self.position, self.phone,
-                             self.folk, self.political_status, self.school, self.position, self.phone)
+        Database.data_handle(sql, *self.to_list())
+
+    def to_list(self):
+        return [self.user_id, self.user_type, self.name, self.p_id, self.gender, self.birthday,
+                self.birth_place, self.folk, self.political_status, self.school, self.position, self.phone,
+                self.name, self.p_id, self.gender, self.birthday, self.birth_place, self.folk, self.political_status,
+                self.school, self.position, self.phone]
 
 
 if __name__ == '__main__':
     Database.initialize()
-    Teacher.create_teacher('20031255', 'XiaoHei', None)
+    print(Teacher.read_teacher('20031252'))
     Database.close()
