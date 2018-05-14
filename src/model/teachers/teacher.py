d433beee24db989d3b94b9d93fc231b063e2a026
JohannLiebert1130 <672016243@qq.com>
2018-05-14 17:24:49 +0800

    forgot to import User

--- src/model/teachers/teacher.py
+++ src/model/teachers/teacher.py
@@ -1,8 +1,7 @@
-class Person(object):
-    pass
+from src.model.users.user import User
 
 
-class Teacher(Person):
+class Teacher(User):
     def __init__(self, user_id, user_type, name, p_id, folk=None, political_status=None,
                  school=None, position=None, phone=None):
         super.__init__(user_id, user_type, name, p_id, folk, political_status, phone)
