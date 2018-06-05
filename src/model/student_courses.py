653eb2a2404f285086dffe978be88672af2f89bf
JohannLiebert1130 <672016243@qq.com>
2018-06-05 16:44:58 +0800

    only extract the information of student_data we are interested in.

--- src/model/student_courses.py
+++ src/model/student_courses.py
@@ -2,7 +2,7 @@ from src.common.database import Database
 from src.model.students.student import Student
 
 
-def get_students(course_key):
+def get_students_data(course_key):
     Database.initialize()
 
     sql = 'select * from student_courses where course_id= %s'
@@ -12,5 +12,13 @@ def get_students(course_key):
     students_data = list()
     for row in data:
         student_key = row[1]
-        student_data = Student.read_student(student_key)
+        temp = Student.read_student(student_key)
+        student_data = list()
+        student_data.append(temp[2])
+        student_data.append(temp[1])
+        student_data.append(temp[9])
+        student_data.append(temp[11])
+        student_data.append(temp[12])
         students_data.append(student_data)
+
+    return students_data
