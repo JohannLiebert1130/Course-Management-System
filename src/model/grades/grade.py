4265501ece43cba6605aeb9fca56d7dcf1a3a020
JohannLiebert1130 <672016243@qq.com>
2018-06-07 13:16:31 +0800

    modify save_grade

--- src/model/grades/grade.py
+++ src/model/grades/grade.py
@@ -2,40 +2,15 @@ import pymysql
 
 from src.common.database import Database
 from src.common.utils import Utils
-from src.model.students.student import Student
 
 
 class Grade:
 
     @staticmethod
-    def get_students_data(course_key):
-        Database.initialize()
-
-        sql = 'select * from grades where course_key= %s'
-        data = Database.query(sql, course_key)
-        print(data)
-
-        students_data = list()
-        for row in data:
-            student_key = row[1]
-            temp = Student.read_student(student_key)
-            student_data = list()
-            student_data.append(temp[2])
-            student_data.append(temp[1])
-            student_data.append(temp[9])
-            student_data.append(temp[11])
-            student_data.append(temp[12])
-            student_data.append(row[3])
-            students_data.append(student_data)
-
-        return students_data
-
-    @staticmethod
     def save_grade(course_id, student_id, grade):
         sql = """
                   update grades set grade = %s
-                  where grades.course_key=(SELECT id FROM courses WHERE course_id = %s) and
-                  grades.student_key=(SELECT id FROM students WHERE user_id = %s)
+                  where course_id = %s and student_id = %s
             """
         try:
             Database.data_handle(sql, grade, course_id, student_id)
@@ -48,8 +23,7 @@ class Grade:
 
         sql = """
                   update grades set grade = %s
-                  where grades.course_key=(SELECT id FROM courses WHERE course_id = %s) and
-                  grades.student_key=(SELECT id FROM students WHERE user_id = %s)
+                  where course_id = %s and student_id = %s
                 """
         try:
             for course_id, student_id, grade in zip(course_ids, student_ids, grades):
